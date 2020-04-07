6679ef2dc673c750592e9b00ee9c6d3a9a0d3f48
joseph.clayton750 <59981818+jbclayton750@users.noreply.github.com>
2020-04-07 12:19:59 -0500

    Update Program.cs

--- PersonalProjectLab/PersonalProjectLab/Program.cs
+++ PersonalProjectLab/PersonalProjectLab/Program.cs
@@ -9,17 +9,14 @@ namespace PersonalProjectLab
             bool repeat = true;
             string input;
             int quit;
-            input = Console.ReadLine();
-            quit = int.Parse(input);
 
-                       
+            Console.WriteLine("Welcome to Clayton Financial ATM's");
+
             while (repeat)
             {
-                Console.WriteLine("Welcome to Clayton Financial ATM's");
-
-
                 Console.WriteLine("Enter 1 to contine or 0 to quit");
-                Console.ReadLine();
+                input = Console.ReadLine();
+                quit = int.Parse(input);
 
                 if (quit == 0)
                 {
