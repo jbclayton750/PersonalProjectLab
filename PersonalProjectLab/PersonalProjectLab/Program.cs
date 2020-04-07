6a25af0ab967797f0975140a734943d930c2c0db
joseph.clayton750 <59981818+jbclayton750@users.noreply.github.com>
2020-04-07 12:37:23 -0500

    Update Program.cs

--- PersonalProjectLab/PersonalProjectLab/Program.cs
+++ PersonalProjectLab/PersonalProjectLab/Program.cs
@@ -8,12 +8,29 @@ namespace PersonalProjectLab
         {
             bool repeat = true;
             string input;
+            string withdraw = "W";
+            string deposit = "D";
+            string decision;
             int quit;
 
             Console.WriteLine("Welcome to Clayton Financial ATM's");
 
+
             while (repeat)
             {
+                Console.WriteLine("Your current balance is $500. Enter W to Withdraw or D to Deposit");
+                decision = Console.ReadLine();
+
+                if (decision.Equals(withdraw))
+                {
+                    Console.WriteLine("Select the amount you would like to withdraw to the nearest whole dollar");
+                }
+
+                if (decision.Equals(deposit))
+                {
+                    Console.WriteLine("Select the amount you wouls like to deposit to the nearest whole dollar");
+                }
+
                 Console.WriteLine("Enter 1 to contine or 0 to quit");
                 input = Console.ReadLine();
                 quit = int.Parse(input);
