fcb68514d02fc5d767964059cc6cbba0bc91ae93
joseph.clayton750 <59981818+jbclayton750@users.noreply.github.com>
2020-04-08 23:36:38 -0500

    testbasics

---
+++ PersonalProjectLab/PersonalProjectLabTests/AtmTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PersonalProjectLabTests
+{
+    [TestClass]
+    public class AtmTests
+    {
+        [TestMethod]
+        public void WithdrawExample()
+        {
+            //arrange
+            //act
+            //assert
+        }
+
+        [TestMethod]
+        public void DepositExample()
+        {
+            //arrange
+            //act
+            //assert
+        }
+    }
+}
